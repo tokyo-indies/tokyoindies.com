d31461f7db32622d255841e8c90573ee4c0aa452
Paul O'Leary McCann <polm@dampfkraft.com>
2026-06-09 23:11:56 +0900

    Add Streamlit tool for post generation
    This refactors the lineup script and gives it a Streamlit frontend.

--- scripts/lineup.py
+++ scripts/lineup.py
@@ -2,17 +2,20 @@
 # generate social media posts
 import csv
 import sys
+import re
+
 
 def remap(row):
 
-    remapper = {"プレゼンター名": "name",
-                "作品名": "title",
-                "BlueSky": "bluesky",
-                "作者X(Twitter)など": "twitter",
-                "作者のDiscordユーザー名": "discord",
-                "作品ホームページ": "homepage",
-                "PV・紹介動画(可能ならYouTubeで)": "pv",
-                }
+    remapper = {
+        "プレゼンター名": "name",
+        "作品名": "title",
+        "BlueSky": "bluesky",
+        "作者X(Twitter)など": "twitter",
+        "作者のDiscordユーザー名": "discord",
+        "作品ホームページ": "homepage",
+        "PV・紹介動画(可能ならYouTubeで)": "pv",
+    }
 
     out = {}
     for key, val in remapper.items():
@@ -20,43 +23,46 @@ def remap(row):
 
     # fix socials
     out["twitter"] = out["twitter"].replace("https://x.com/", "@")
+    # this is old but people still give it to us sometimes
+    out["twitter"] = out["twitter"].replace("https://twitter.com/", "@")
     out["bluesky"] = out["bluesky"].replace("https://bsky.app/profile/", "@")
     # If someone puts a username like "@user" that doesn't work
     if out["bluesky"] is not None and "." not in out["bluesky"]:
         out["bluesky"] = out["bluesky"] + ".bsky.social"
     out["discord"] = ("@" + out["discord"]) if out["discord"] else ""
+
+    # remove non-url values like "will send later"
+    for key in ("homepage", "pv"):
+        if not re.match("https?://", out[key]):
+            out[key] = ""
     return out
 
 
-def main():
-
-    # 0. read in tsv
+def read_tsv(tsvfile):
     presentations = []
-    with open(sys.argv[1]) as tsvfile:
-        reader = csv.DictReader(tsvfile, delimiter="\t")
-        for row in reader:
-            if row["Status"] != "Confirmed":
-                continue
-            presentations.append(remap(row))
+    reader = csv.DictReader(tsvfile, delimiter="\t")
+    for row in reader:
+        if row["Status"] != "Confirmed":
+            continue
+        presentations.append(remap(row))
 
-    # 1. twitter
-    print("----- twitter -----")
-    print("今月の作品:")
-    print()
+    return presentations
 
+
+def post_twitter(intro, presentations):
+    out = intro + "\n\n"
     for presen in presentations:
         title = presen["title"]
         handle = presen["twitter"]
         if not handle:
             handle = "/ " + presen["name"]
-        print(f"- {title} {handle}")
+        out += f"- {title} {handle}\n"
+
+    return out
 
 
-    # 2. bluesky
-    print()
-    print("----- bluesky -----")
-    print("今月の作品:")
-    print()
+def post_bluesky(intro, presentations):
+    out = intro + "\n\n"
 
     for presen in presentations:
         title = presen["title"]
@@ -65,14 +71,14 @@ def main():
         handle = presen.get("bluesky")
         if not handle:
             handle = "/ " + presen["name"]
-        
-        print(f"- {title} {handle}")
 
-    # 3. discord
-    print()
-    print("----- discord -----")
-    print("今月の作品:")
-    print()
+        out += f"- {title} {handle}\n"
+
+    return out
+
+
+def post_discord(intro, presentations):
+    out = intro + "\n\n"
 
     for presen in presentations:
         title = presen["title"]
@@ -81,12 +87,13 @@ def main():
             title = f"[{title}]({link})"
         if pv := presen["pv"]:
             handle += f" [(PV)]({pv})"
-        print(f"- {title} {handle}")
-    
-    print()
-    print("----- html -----")
-    print("今月の作品:")
-    print()
+        out += f"- {title} {handle}\n"
+
+    return out
+
+
+def post_html(intro, presentations):
+    out = intro + "\n\n"
 
     for presen in presentations:
         title = presen["title"]
@@ -95,7 +102,38 @@ def main():
             title = f"[{title}]({link})"
         if pv := presen["pv"]:
             handle += f" [(PV)]({pv})"
-        print(f"- {title} {handle}")
+        out += f"- {title} {handle}\n"
+
+    return out
+
+
+def main():
+
+    intro = "今月の紹介作品:"
+
+    # 0. read in tsv
+    with open(sys.argv[1]) as tsvfile:
+        presentations = read_tsv(tsvfile)
+
+    # 1. twitter
+    print("----- twitter -----")
+    print(post_twitter(intro, presentations))
+
+    # 2. bluesky
+    print()
+    print("----- bluesky -----")
+    print(post_bluesky(intro, presentations))
+
+    # 3. discord
+    print()
+    print("----- discord -----")
+
+    print(post_discord(intro, presentations))
+
+    print()
+    print("----- html -----")
+    print(post_html(intro, presentations))
+
 
 if __name__ == "__main__":
     main()
---
+++ scripts/web-tool.py
@@ -0,0 +1,34 @@
+import csv
+from io import StringIO
+
+import streamlit as st
+
+import lineup
+
+st.write(
+    """
+# Tokyo Indies SNS Post Tool
+
+To use this tool, make sure the presenter application spreadsheet has presenters for this month marked as Confirmed in the Status column. Then download the sheet as a TSV file and upload it here. Content to post on each SNS will show up below."""
+)
+
+upfile = st.file_uploader("Upload Presenter CSV")
+
+if upfile is not None:
+
+    strio = StringIO(upfile.getvalue().decode("utf-8"))
+    pres = lineup.read_tsv(strio)
+    intro = st.text_input("Intro text", "今月の紹介作品:")
+
+    st.write("# Twitter")
+    st.code(lineup.post_twitter(intro, pres), language="markdown")
+    "---"
+    st.write("# BlueSky")
+    st.write("To post Markdown on BlueSky, use [deck.blue](https://deck.blue).")
+    st.code(lineup.post_bluesky(intro, pres), language="markdown")
+    "---"
+    st.write("# Discord")
+    st.code(lineup.post_discord(intro, pres), language="markdown")
+    "---"
+    st.write("# HTML (blog post)")
+    st.code(lineup.post_html(intro, pres), language="markdown")
